7b16f5101361862c526deaf169696edb936fc7a0
Agata Raap <no.face.at.facebook+github@gmail.com>
2023-10-07 21:49:05 +0200

    add collision border to map

--- game/scenes/tileset/tilemap.gd
+++ game/scenes/tileset/tilemap.gd
@@ -12,22 +12,7 @@ var bushes = 13
 var bushes_snall = 14
 var trees = 15
 
-# Custom Datas
-var is_breakable = "breakable"
-
-var player : Node2D
-
-func _ready():
-    get_tree().get_current_scene().child_entered_tree.connect(_on_child_entered_tree)
-    _scatter_objects()
-
-func _unhandled_input(_event):
-    if player && Input.is_action_just_pressed('action'):
-        var action_map_position = local_to_map(player.global_position)
-        _break_nearest_crystal(action_map_position)
-
-func _break_nearest_crystal(pos: Vector2):
-    var neighbours = [
+var neighbours = [
         Vector2.UP,
         Vector2.UP + Vector2.LEFT,
         Vector2.LEFT,
@@ -36,8 +21,23 @@ func _break_nearest_crystal(pos: Vector2):
         Vector2.DOWN + Vector2.RIGHT,
         Vector2.RIGHT,
         Vector2.RIGHT + Vector2.UP
-    ]
+]
 
+# Custom Datas
+var is_breakable = "breakable"
+
+var player : Node2D
+
+func _ready():
+    get_tree().get_current_scene().child_entered_tree.connect(_on_child_entered_tree)
+    _complete_map()
+
+func _unhandled_input(_event):
+    if player && Input.is_action_just_pressed('action'):
+        var action_map_position = local_to_map(player.global_position)
+        _break_nearest_crystal(action_map_position)
+
+func _break_nearest_crystal(pos: Vector2):
     for neighbour in neighbours:
         var position_to_check = pos + neighbour
         var tile_data : TileData = get_cell_tile_data(object_layer, position_to_check)
@@ -47,13 +47,26 @@ func _break_nearest_crystal(pos: Vector2):
             G.crystal_broken.emit()
             break
 
-func _scatter_objects():
+func _complete_map():
     var tiles = get_used_cells(ground_layer)
+    _scatter_objects(tiles)
+    _add_collision_border(tiles)
+
+func _scatter_objects(tiles: Array):
     var objects = [crystal_terrain, bushes, bushes_snall, trees]
     var object_count = tiles.size() / 4 # 25% of map are poulated
     for i in object_count:
-        set_cell(object_layer, tiles[randi() % tiles.size()], objects[randi() % objects.size()], Vector2.ZERO)
-    pass
+        var source_id = objects[randi() % objects.size()]
+        var coords = tiles[randi() % tiles.size()]
+        set_cell(object_layer, coords, source_id, Vector2.ZERO)
+        if source_id == crystal_terrain:
+            set_cell(effects_layer, coords, source_id, Vector2.DOWN)
+
+func _add_collision_border(tiles):
+    for tile in tiles:
+        for neighbour in neighbours:
+            if get_cell_source_id(ground_layer, tile + Vector2i(neighbour)) == -1:
+                set_cell(ground_layer, tile + Vector2i(neighbour), grass, Vector2(9, 1))
 
 func _on_child_entered_tree(node: Node):
     if node.get_groups().has('kitty'):
