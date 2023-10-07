25c012a7a9822c0796d54f03381b2fd27622aeed
Agata Raap <no.face.at.facebook+github@gmail.com>
2023-10-07 13:20:59 +0200

    break crystals near player

--- game/scenes/tileset/tilemap.gd
+++ game/scenes/tileset/tilemap.gd
@@ -17,11 +17,28 @@ func _ready():
 
 func _unhandled_input(_event):
     if player && Input.is_action_just_pressed('action'):
-        var action_map_position = local_to_map(get_global_mouse_position())
-        var tile_data : TileData = get_cell_tile_data(object_layer, action_map_position)
+        var action_map_position = local_to_map(player.global_position)
+        _break_nearest_crystal(action_map_position)
+
+func _break_nearest_crystal(position: Vector2):
+    var neighbours = [
+        Vector2.UP,
+        Vector2.UP + Vector2.LEFT,
+        Vector2.LEFT,
+        Vector2.LEFT + Vector2.DOWN,
+        Vector2.DOWN,
+        Vector2.DOWN + Vector2.RIGHT,
+        Vector2.RIGHT,
+        Vector2.RIGHT + Vector2.UP
+    ]
+
+    for neighbour in neighbours:
+        var position_to_check = position + neighbour
+        var tile_data : TileData = get_cell_tile_data(object_layer, position_to_check)
         if tile_data && tile_data.get_custom_data(is_breakable):
-            set_cell(object_layer, action_map_position, -1)
-            set_cell(effects_layer, action_map_position, -1)
+            set_cell(object_layer, position_to_check, -1)
+            set_cell(effects_layer, position_to_check, -1)
+            break
 
 func _on_child_entered_tree(node: Node):
     if node.get_groups().has('kitty'):
